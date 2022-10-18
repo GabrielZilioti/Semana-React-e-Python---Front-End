d136f95cdb77ba96b625266791e675caf5fbeb1b
Gabriel Zilioti <gazilioti@hotmail.com>
2022-10-17 22:24:33 -0300

    fix: Correção cabecalho

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Cabecalho from '../src/components/Cabecalho/Cabecalho'
-import { CabecalhoContainer, Logo } from '../src/components/Cabecalho/Cabecalho.style'
 
 const Home: NextPage = () => {
   return (
